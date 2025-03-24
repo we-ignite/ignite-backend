90aa42c78f95378930522b5cd232ef798960acfe
ajaywagh955 <waghajay304@gmail.com>
2025-03-25 02:13:28 +0530

    new App Added For Admin Dashboard and Login functionality for Admin Implemented

---
+++ dashboard/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+# Create your views here.
+
+# -------------------------- Code for Login the Admin Dashboard (starts) --------------------------
+
+def adminLogin(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username=username).first()
+        if user_obj is None:
+            messages.success(request, "User Not Found")
+            return redirect('admin-login')
+
+        user = authenticate(username=username,password=password)
+        if user is None:
+            messages.success(request, "Wrong Password")
+            return redirect('admin-login')
+
+        login(request,user)
+        return redirect('index')
+    return render(request,"dashboard/login.html")
+
+# -------------------------- Code for Login the Admin Dashboard (end) --------------------------
+
+
+# -------------------------- Code for Admin Dashboard (starts) --------------------------
+
+
+@login_required(login_url="admin-login")
+def index(request):
+    return JsonResponse({"message":"Your Dashboard is ready"})
+
+
+
+# -------------------------- Code for Admin Dashboard (starts) --------------------------
--- ignite_backend/settings.py
+++ ignite_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Installed Apps
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'ignite_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INSTALLED_APPS += ['corsheaders']
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
+CORS_ALLOW_ALL_ORIGINS = True
