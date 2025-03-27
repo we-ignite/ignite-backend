ed60bc2274fb4c50cd4f012aa0b01880ebb067ef
ajaywagh955 <waghajay304@gmail.com>
2025-03-27 23:50:56 +0530

    Member Image Changed

--- ignite_backend/settings.py
+++ ignite_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cjj5#tg=!^&r6^m0bi+iok1vrf#os_=0a7a2)t)-7coyvft*8=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ignite.internzze.com", "www.ignite.internzze.com"]
+ALLOWED_HOSTS = ["ignite.internzze.com", "www.ignite.internzze.com",'*']
 
 
 # Application definition
