ee119ad27cee6e0e09acbef8eb6dd16e2f37f0c7
ajaywagh955 <waghajay304@gmail.com>
2025-03-28 23:50:14 +0530

    DB updated

--- ignite_backend/settings.py
+++ ignite_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os   
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +86,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://neondb_owner:npg_MHtovma6l3LR@ep-morning-glade-a1otkgji-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
+    )
 }
 
 
