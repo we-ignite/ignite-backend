a955b121706c7b83246154a5ec951a4cc8f36654
ajaywagh955 <waghajay304@gmail.com>
2025-03-29 12:51:40 +0530

    DB changed

--- ignite_backend/settings.py
+++ ignite_backend/settings.py
@@ -88,6 +88,19 @@ DATABASES = {
 }
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ignite',
+        'USER': 'ignite',
+        'PASSWORD': 'ignite2025',
+        'HOST': 'ignite-db.czaqoy4qcn93.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
